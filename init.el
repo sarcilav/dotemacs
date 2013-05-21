918bd3b2183cc9ad11d92da3c8a5856a315bd854
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2013-05-20 22:41:37 -0500

    Improve ido mode by with flex-matching and some other bits

--- init.el
+++ init.el
@@ -7,7 +7,11 @@
 ;; Start ido
 (require 'ido)
 (ido-mode t)
-
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point t
+      ido-max-prospects 10)
 
 
 ;; add marmalade

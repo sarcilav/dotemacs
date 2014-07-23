e8bff8be39c6d95f2bb5b17eb8abbaf7fe7c3f54
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2014-07-23 11:26:58 -0700

    update elpa folder and face.el

---
+++ elpa/handlebars-sgml-mode-0.1.0/handlebars-sgml-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; handlebars-sgml-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("handlebars-sgml-mode-pkg.el" "handlebars-sgml-mode.el")
+;;;;;;  (21388 65323 180458 0))
+
+;;;***
+
+(provide 'handlebars-sgml-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; handlebars-sgml-mode-autoloads.el ends here
---
+++ elpa/handlebars-sgml-mode-0.1.0/handlebars-sgml-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "handlebars-sgml-mode" "0.1.0" "Add Handlebars contextual indenting support to sgml-mode" (quote nil))

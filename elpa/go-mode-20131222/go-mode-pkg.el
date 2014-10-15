3cdd4ecec11f910a289715a6866420a956c8ed5d
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2014-10-14 23:31:43 -0500

    go mode

---
+++ elpa/go-mode-20131222/go-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-mode" "20131222" "Major mode for the Go programming language" 'nil)
--- personal/hooks.el
+++ personal/hooks.el
@@ -1,2 +1,10 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
+
+;; Display spaces on go-mode as using 2 width spaces
+(add-hook 'go-mode-hook
+  (lambda ()
+    (setq-default)
+    (setq tab-width 2)
+    (setq standard-indent 2)
+    (setq indent-tabs-mode nil)))

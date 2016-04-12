801a30b5bdef67ad9011aedc2364a438ce044e50
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2016-04-12 11:04:25 -0500

    Upgrade clojure-mode, yasnippet and others.

--- elpa/yasnippet-0.9.0.1/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "0.9.0.1" "Yet another snippet extension for Emacs." 'nil :url "http://github.com/capitaomorte/yasnippet" :keywords '("convenience" "emulation"))
---
+++ elpa/yasnippet-0.9.1/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yasnippet" "0.9.1" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :keywords '("convenience" "emulation") :url "http://github.com/capitaomorte/yasnippet")

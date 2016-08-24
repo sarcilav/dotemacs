dbbf843323d0ffa1362685996e30581cdd864d94
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2016-08-24 15:51:50 -0500

    Upgrade with-editor 2.5.1 to 2.5.2

--- elpa/with-editor-2.5.2/with-editor-autoloads.el
+++ elpa/with-editor-2.5.2/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22303 53491 739212 0))
+;;;;;;  (22462 1798 725684 0))
 
 ;;;***
 

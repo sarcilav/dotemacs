3bea6e275d787a7e1aa671eb7ed94a197fcad7c3
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2016-10-27 15:27:46 -0500

    Upgrade yasnippet

--- elpa/yasnippet-0.10.0/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "0.10.0" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :keywords '("convenience" "emulation") :url "http://github.com/capitaomorte/yasnippet")

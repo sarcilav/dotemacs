ca2e0b333098462cf4c8bd1217ca1e3c681786e4
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2018-03-04 20:59:00 +0100

    Update cider, magit and yasnippet
    And other minor modes

---
+++ elpa/web-mode-15.1/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "15.1" "major mode for editing web templates" '((emacs "23.1")) :commit "e8a7029de04013b9cb5f1499c1b36d4feac7839f" :url "http://web-mode.org" :keywords '("languages"))

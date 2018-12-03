4f970850381f25143edb4d4de6d571238afc8d9d
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2018-12-03 16:21:42 +0100

    Ugrade magit and other goodies

---
+++ elpa/treepy-1.0.0/treepy-autoloads.el
@@ -0,0 +1,15 @@
+;;; treepy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("treepy.el") (23557 18634 356041 611000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; treepy-autoloads.el ends here

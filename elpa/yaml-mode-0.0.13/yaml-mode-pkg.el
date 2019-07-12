fbcf31a51d7219bc1d1916c2c3d9c6cd2734747f
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2019-07-12 10:40:15 +0200

    bump yaml-mode

--- elpa/yaml-mode-0.0.13/yaml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yaml-mode" "0.0.13" "Major mode for editing YAML files" '((emacs "24.1")) :keywords '("data" "yaml"))
--- elpa/yaml-mode-0.0.14/yaml-mode-autoloads.el
+++ elpa/yaml-mode-0.0.14/yaml-mode-autoloads.el
@@ -1,7 +1,10 @@
 ;;; yaml-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (0 0 0 0))
 ;;; Generated autoloads from yaml-mode.el
@@ -25,5 +28,6 @@ Simple mode to edit YAML.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; yaml-mode-autoloads.el ends here

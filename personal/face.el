06043b3670052c1617654f4a6057b7574c2d7953
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2020-01-30 14:07:37 +0100

    Subtle system bell

--- personal/face.el
+++ personal/face.el
@@ -77,3 +77,12 @@
    "Sets the transparency of the frame window. 0=transparent/100=opaque"
    (interactive "nTransparency Value 0 - 100 opaque:")
    (set-frame-parameter (selected-frame) 'alpha value))
+
+;; Subtle ring bell
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
